8cef83ed591bf9d24481d2760b09a63b13147130
Pete McWilliams <pmcwilliams@augustash.com>
2012-03-23 17:40:27 -0500

    Restoring default script

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -13,11 +13,6 @@ fpath=($ZSH/functions $ZSH/completions $fpath)
 # TIP: Add files you don't want in git to .gitignore
 for config_file ($ZSH/lib/*.zsh) source $config_file
 
-# Load all of the config files in ~/.dotfiles that end in .zsh
-for config_file in $(find $ZSHDOTS -type d \( -name zsh \) -prune -o -name "*.zsh" -print); do
-    source $config_file
-done
-
 # Set ZSH_CUSTOM to the path where your custom config files
 # and plugins exists, or else we will use the default custom/
 if [[ -z "$ZSH_CUSTOM" ]]; then
@@ -78,4 +73,3 @@ else
     fi
   fi
 fi
-
